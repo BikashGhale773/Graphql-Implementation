afef1101905ee8b166b1ae401c0012f76a8aa28c
BikashGhale773 <ghalebikash445@gmail.com>
2023-10-01 23:02:21 -0500

    complete functioning graphql API

--- src/main/java/com/example/graphqlproject/GraphqlProjectApplication.java
+++ src/main/java/com/example/graphqlproject/GraphqlProjectApplication.java
@@ -1,10 +1,17 @@
 package com.example.graphqlproject;
 
+import com.example.graphqlproject.Entity.Book;
+import com.example.graphqlproject.Service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GraphqlProjectApplication {
+public class GraphqlProjectApplication implements CommandLineRunner {
+
+	@Autowired
+	private BookService bookService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(GraphqlProjectApplication.class, args);
@@ -13,4 +20,31 @@ public class GraphqlProjectApplication {
 
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Book b1 = new Book();
+		b1.setAuthor("Bikash");
+		b1.setDesc("Graphql");
+		b1.setTitle("Graphql iplementation");
+		b1.setPrice(300);
+		b1.setPages(25);
+
+		Book b2 = new Book();
+		b2.setAuthor("Kamala");
+		b2.setDesc("Java");
+		b2.setTitle("Java iplementation");
+		b2.setPrice(200);
+		b2.setPages(20);
+
+		Book b3 = new Book();
+		b3.setAuthor("Kusum");
+		b3.setDesc("Springboot");
+		b3.setTitle("Springboot iplementation");
+		b3.setPrice(100);
+		b3.setPages(13);
+
+		this.bookService.create(b1);
+		this.bookService.create(b2);
+		this.bookService.create(b3);
+	}
 }
--- src/main/java/com/example/graphqlproject/controller/BookController.java
+++ src/main/java/com/example/graphqlproject/controller/BookController.java
@@ -2,35 +2,64 @@ package com.example.graphqlproject.controller;
 
 import com.example.graphqlproject.Entity.Book;
 import com.example.graphqlproject.Service.BookService;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/books")
+//@RestController
+//@RequestMapping("/books")
+
+@Controller
 public class BookController {
-
-
     @Autowired
     private BookService bookService;
 
-    @PostMapping
+//    @PostMapping
     //create
-    public Book create(@RequestBody Book book){
-        return this.bookService.create(book);
+    //@RequestBody to pass data now it is converted into @Arguments
+    @MutationMapping("createBook")
+    public Book create(@Argument BookInput book){
+        Book b5 = new Book();
+        b5.setPages(book.getPages());
+        b5.setPrice(book.getPrice());
+        b5.setAuthor(book.getAuthor());
+        b5.setDesc(book.getDesc());
+        b5.setTitle(book.getTitle());
+        return this.bookService.create(b5);
     }
 
 
-    @GetMapping
+//    @GetMapping
     //get all
-    public List<Book> getAll(){
+    @QueryMapping("allBooks")
+    public List<Book> getBooks(){
         return this.bookService.getAll();
     }
 
-    @GetMapping("/{bookId}")
+//    @GetMapping("/{bookId}")
     //get single
-    public Book getBookById(@PathVariable int bookId){
-        return this.bookService.get(bookId);
+    //@PathVariable to get dynamic id now it is converted into @Arguments
+
+    @QueryMapping("singleBook")
+    public Book getBookById(@Argument int bookId){
+       return this.bookService.get(bookId);
     }
+}
+
+@Getter
+@Setter
+class BookInput{
+    private String title;
+    private String desc;
+    private String author;
+    private double price;
+    private int pages;
+
 }
